5879691f00da59f8d56a570e7e208132bf599c14
Julius Mehringer <julius.mehringer@iis.fraunhofer.de>
2025-05-21 10:56:01 +0200

    try sudo

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -1,2 +1,2 @@
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
