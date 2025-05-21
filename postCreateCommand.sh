71846d397f58e2ed5f704519dabb3f6ce1d4c7eb
Julius Mehringer <julius.mehringer@iis.fraunhofer.de>
2025-05-21 11:21:13 +0200

    omit chrome installation, not needed per default

--- postCreateCommand.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e  # Exit immediately if a command exits with a non-zero status
-
-# Download the .deb file
-sudo wget -O google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-# Install the .deb file
-sudo dpkg -i google-chrome-stable_current_amd64.deb
