d265fe4c1d666edc692bd98ce1dd3036a9d7734c
Julius Mehringer <julius.mehringer@iis.fraunhofer.de>
2025-05-21 11:11:09 +0200

    update createcommand

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Download the .deb file
+sudo wget -O google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Install the .deb file
 sudo dpkg -i google-chrome-stable_current_amd64.deb
